14c00947ef0b0c62a4f5bd13d1ac1d49542cefd9
n0an <nantog@gmail.com>
2016-04-14 21:09:23 +0300

    rus cat names added

--- Nickname generator/ANNamesFactory.m
+++ Nickname generator/ANNamesFactory.m
@@ -33,9 +33,9 @@
     dispatch_once(&onceToken, ^{
         sharedFactory = [[ANNamesFactory alloc] init];
         
-        ANNameCategory* area01cat01 = [[ANNameCategory alloc] initWithCategoryID:@"01.01" andCategoryTitle:@"Greek Mythology" andCategoryImageName:@"bg06" andCategoryBackgroundImageName:@"bg03" andAlias:@"MythGreek"];
-        ANNameCategory* area01cat02 = [[ANNameCategory alloc] initWithCategoryID:@"01.02" andCategoryTitle:@"Vedic Mythology" andCategoryImageName:@"vedicBg21_1920" andCategoryBackgroundImageName:@"vedicBg10_2437" andAlias:@"MythVedic"];
-        ANNameCategory* area01cat03 = [[ANNameCategory alloc] initWithCategoryID:@"01.03" andCategoryTitle:@"Roman Mythology" andCategoryImageName:@"romanBg14_4592" andCategoryBackgroundImageName:@"romanBg07_7784" andAlias:@"MythRoman"];
+        ANNameCategory* area01cat01 = [[ANNameCategory alloc] initWithCategoryID:@"01.01" andCategoryTitle:@"Греческая мифология" andCategoryImageName:@"bg06" andCategoryBackgroundImageName:@"bg03" andAlias:@"MythGreek"];
+        ANNameCategory* area01cat02 = [[ANNameCategory alloc] initWithCategoryID:@"01.02" andCategoryTitle:@"Ведическая мифология" andCategoryImageName:@"vedicBg21_1920" andCategoryBackgroundImageName:@"vedicBg10_2437" andAlias:@"MythVedic"];
+        ANNameCategory* area01cat03 = [[ANNameCategory alloc] initWithCategoryID:@"01.03" andCategoryTitle:@"Римская мифология" andCategoryImageName:@"romanBg14_4592" andCategoryBackgroundImageName:@"romanBg07_7784" andAlias:@"MythRoman"];
    
         sharedFactory.namesCategories = @[area01cat01, area01cat02, area01cat03];
         
