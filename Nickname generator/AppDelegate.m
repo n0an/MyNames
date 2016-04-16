ae5b7f24d3adb1a7eac676b0c09a03660cd23158
n0an <nantog@gmail.com>
2016-04-16 22:06:22 +0300

    appDidEnterBackgroud. coredata save

--- Nickname generator/AppDelegate.m
+++ Nickname generator/AppDelegate.m
@@ -22,7 +22,7 @@
     
     UIColor *tabBarColor = RGBA(201, 81, 0, 255);
     [[UITabBar appearance] setTintColor:tabBarColor];
-
+    
     
     
     return YES;
@@ -36,6 +36,8 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    [[ANDataManager sharedManager] saveContext];
+
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
