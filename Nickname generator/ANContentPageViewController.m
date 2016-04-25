53dd9beed4eedef18921530d74169a1f6262f707
n0an <nantog@gmail.com>
2016-04-25 19:36:01 +0300

    ContentPageVC: design and constrs added

---
+++ Nickname generator/ANContentPageViewController.m
@@ -0,0 +1,37 @@
+//
+//  ANContentPageViewController.m
+//  Nickname generator
+//
+//  Created by Anton Novoselov on 25/04/16.
+//  Copyright © 2016 Anton Novoselov. All rights reserved.
+//
+
+#import "ANContentPageViewController.h"
+
+@interface ANContentPageViewController ()
+
+@end
+
+@implementation ANContentPageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
