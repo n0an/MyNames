de2ca2fce49ccaffb0c07d6ad3e2c801f682bcd4
n0an <nantog@gmail.com>
2016-04-25 19:15:43 +0300

    page view controller added

---
+++ Nickname generator/ANPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  ANPageViewController.h
+//  Nickname generator
+//
+//  Created by Anton Novoselov on 25/04/16.
+//  Copyright © 2016 Anton Novoselov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ANPageViewController : UIPageViewController
+
+@end
