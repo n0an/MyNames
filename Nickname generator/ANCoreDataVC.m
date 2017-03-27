aceb68dd1c46036cb99f385b32fe99f682445e8c
n0an <nantog@gmail.com>
2017-03-27 11:28:24 +0300

    table view row actions added for ANFaforiteNamesVC

--- Nickname generator/ANCoreDataVC.m
+++ Nickname generator/ANCoreDataVC.m
@@ -8,6 +8,7 @@
 
 #import "ANCoreDataVC.h"
 #import "ANDataManager.h"
+#import "ANUtils.h"
 
 @interface ANCoreDataVC ()
 
@@ -155,10 +156,9 @@
         
         NSError *error = nil;
         if (![context save:&error]) {
-            // Replace this implementation with code to handle the error appropriately.
-            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            
             NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-            abort();
+            postNotificationFatalCoreDataError();
         }
         
         
--- Nickname generator/ANFavoriteNamesVC.m
+++ Nickname generator/ANFavoriteNamesVC.m
@@ -23,6 +23,8 @@
 
 #import "ANRotateTransitionAnimator.h"
 
+#import <Social/Social.h>
+
 @interface ANFavoriteNamesVC ()
 
 @property (strong, nonatomic) NSString* searchPredicateString;
@@ -378,8 +380,32 @@
     
 }
 
+- (void) showAlertShareErrorWithTitle:(NSString *)title andMessage:(NSString *) message {
+    
+    UIAlertController* errorAlertController = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction* okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+    
+    [errorAlertController addAction:okAction];
+    
+    [self presentViewController:errorAlertController animated:true completion:nil];
+    
+    
+}
+
+
+- (void) showActivityVCWithItems:(NSArray *)items {
+    
+    UIActivityViewController* activityVC = [[UIActivityViewController alloc] initWithActivityItems:items applicationActivities:nil];
+    
+    activityVC.popoverPresentationController.sourceView = self.view;
+    
+    [self presentViewController:activityVC animated:true completion:nil];
+    
+}
 
 #pragma mark - UITableViewDelegate
+
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     
@@ -394,27 +420,135 @@
         
         [self performSegueWithIdentifier:@"showDescriptionVC" sender:name];
         
-        /*
-        ANDescriptioinVC* vc = [self.storyboard instantiateViewControllerWithIdentifier:@"ANDescriptioinVC"];
         
-        NSString* nameID = name.nameID;
-
-        
-        ANName* originName = [[ANNamesFactory sharedFactory] getNameForID:nameID];
-        
-        vc.namesArray = @[originName];
-        
-        
-        UINavigationController* nav = [[UINavigationController alloc] initWithRootViewController:vc];
-        
-        nav.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
-        
-        [self presentViewController:nav animated:YES completion:nil];
-        */
         
     }
     
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+
+- (nullable NSArray<UITableViewRowAction *> *)tableView:(UITableView *)tableView editActionsForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewRowAction *shareAction = [UITableViewRowAction rowActionWithStyle:UITableViewRowActionStyleDefault title:NSLocalizedString(@"ROW_ACTION_SHARE", nil) handler:^(UITableViewRowAction * _Nonnull action, NSIndexPath * _Nonnull indexPath) {
+        
+        ANFavoriteName* firstName = [self.fetchedResultsController objectAtIndexPath:indexPath];
+
+        
+        NSString* introTextToShare = NSLocalizedString(@"SHARE_TEXT", nil);
+        
+        NSString* fullTextToShare = [NSString stringWithFormat:@"%@ - %@", firstName.nameFirstName, introTextToShare];
+        
+        UIImage* imageToShare = [UIImage imageNamed:firstName.nameImageName];
+        
+        // Presenting action sheet with share options - Facebook, Twitter, UIActivityVC
+        UIAlertController* alertController = [UIAlertController alertControllerWithTitle:nil message:NSLocalizedString(@"SHARE_MESSAGE", nil) preferredStyle:UIAlertControllerStyleActionSheet];
+        
+        // TWITTER ACTION
+        UIAlertAction* twitterAction = [UIAlertAction actionWithTitle:@"Twitter" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            
+            // Check if Twitter is available. Otherwise, display an error message
+            
+            if (![SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter]) {
+                
+                [self showAlertShareErrorWithTitle:NSLocalizedString(@"SHARE_TWITTER_UNAVAILABLE_TITLE", nil) andMessage:NSLocalizedString(@"SHARE_TWITTER_UNAVAILABLE_MESSAGE", nil)];
+                
+                return;
+                
+            }
+            
+            // Display Tweet Composer
+            SLComposeViewController* tweetComposer = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+            
+            [tweetComposer setInitialText:fullTextToShare];
+            [tweetComposer addImage:imageToShare];
+            
+            //[tweetComposer addURL:shareUrl];
+            
+            [self presentViewController:tweetComposer animated:true completion:nil];
+            
+            
+        }];
+        
+        // FACEBOOK ACTION
+        UIAlertAction* facebookAction = [UIAlertAction actionWithTitle:@"Facebook" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            
+            // Check if Facebook is available. Otherwise, display an error message
+            
+            if (![SLComposeViewController isAvailableForServiceType:SLServiceTypeFacebook]) {
+                
+                [self showAlertShareErrorWithTitle:NSLocalizedString(@"SHARE_FACEBOOK_UNAVAILABLE_TITLE", nil) andMessage:NSLocalizedString(@"SHARE_FACEBOOK_UNAVAILABLE_MESSAGE", nil)];
+                
+                return;
+                
+            }
+            
+            // Display Facebook Composer
+            SLComposeViewController* facebookComposer = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
+            
+            [facebookComposer setInitialText:fullTextToShare];
+            [facebookComposer addImage:imageToShare];
+            
+            //[facebookComposer addURL:shareUrl];
+            
+            [self presentViewController:facebookComposer animated:true completion:nil];
+            
+        }];
+        
+        // OTHER ACTION - UIActivityVC
+        UIAlertAction* otherAction = [UIAlertAction actionWithTitle:NSLocalizedString(@"SHARE_ACTION_OTHER", nil) style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            
+            NSString* textToShare = firstName.nameFirstName;
+            
+            NSArray* shareItems;
+            
+            if (imageToShare != nil) {
+                shareItems = @[textToShare, imageToShare];
+            } else {
+                shareItems = @[textToShare];
+            }
+            
+            [self showActivityVCWithItems:shareItems];
+            
+        }];
+        
+        // CANCEL ACTION
+        UIAlertAction* cancelAction = [UIAlertAction actionWithTitle:NSLocalizedString(@"CANCEL_CLEAR", nil) style:UIAlertActionStyleCancel handler:nil];
+        
+        [alertController addAction:facebookAction];
+        [alertController addAction:twitterAction];
+        [alertController addAction:otherAction];
+        [alertController addAction:cancelAction];
+        
+        alertController.popoverPresentationController.sourceView = self.view;
+        
+        [self presentViewController:alertController animated:YES completion:nil];
+        
+       
+        
+    }];
+    
+    UITableViewRowAction *deleteAction = [UITableViewRowAction rowActionWithStyle:UITableViewRowActionStyleDefault title:NSLocalizedString(@"ROW_ACTION_DELETE", nil) handler:^(UITableViewRowAction * _Nonnull action, NSIndexPath * _Nonnull indexPath) {
+        
+        NSManagedObjectContext *context = [self.fetchedResultsController managedObjectContext];
+        [context deleteObject:[self.fetchedResultsController objectAtIndexPath:indexPath]];
+        
+        NSError *error = nil;
+        if (![context save:&error]) {
+            
+            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+            postNotificationFatalCoreDataError();
+        }
+        
+        
+        
+    }];
+    
+    shareAction.backgroundColor = RGBA(48, 173, 99, 255);
+    deleteAction.backgroundColor = [UIColor redColor];
+    
+    return @[deleteAction, shareAction];
+    
 }
 
 
